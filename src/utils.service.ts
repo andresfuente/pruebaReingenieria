dcb58eb2d498a794cbf252fff72de4ca90a6a971
Javi B <dbrivies@everis.com>
2016-12-07 16:04:53 +0100

    Added checkFormatEmail function to test an email

--- src/utils.service.ts
+++ src/utils.service.ts
@@ -339,6 +339,16 @@ module OrangeFeSARQ.Services {
 
             return deferred.promise;
         }
+		
+		checkFormatEmail(email: string){
+			let vm = this;
+			let format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+			if (email.match(format)) {
+				return true;
+			}
+			return false;
+			
+		}
 
     }
     angular.module('utils', [])
