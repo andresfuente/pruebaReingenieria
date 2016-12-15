1c01891b9bf66f7e5268ae8ce062dd3d05ead408
jcardonv <N3sy4l1J41m3>
2016-12-15 10:10:47 +0100

    bucketBalance.service.ts se añade la funcion para buscar por documento

--- src/bucketBalance.service.ts
+++ src/bucketBalance.service.ts
@@ -1,4 +1,4 @@
-module OrangeFeSARQ.Services{
+module OrangeFeSARQ.Services {
     'use strict';
 
     export class BucketBalanceSrv extends OrangeFeSARQ.Services.ParentService {
@@ -18,10 +18,10 @@ module OrangeFeSARQ.Services{
             vm.genericConstant = $injector.get('genericConstant');
         }
 
-        getBalanceByMsisdn(msisdn: string, param: string, componentName:string='moreMegasComp'): any {
+        getBalanceByMsisdn(msisdn: string, param: string, componentName: string = 'more-megas-comp'): any {
             let vm = this;
             let _search: any;
-            vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalance;            
+            vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalance;
             _search = {
                 queryParams: {
                     publicKey: msisdn,
@@ -31,18 +31,17 @@ module OrangeFeSARQ.Services{
 
             };
 
-            if(param === 'Line'){
-                _search.queryParams.productBucket = 'line';                
+            if (param === 'Line') {
+                _search.queryParams.productBucket = 'line';
                 //vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalanceLineAPIUrl;
-            }else if(param === 'Bonus'){
+            } else if (param === 'Bonus') {
                 //_search.queryParams.productBucket = 'bonus';
                 //vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalanceBonusAPIUrl;
                 //_search = {};
                 //TO-DO: para llamar a la api
-                _search.queryParams.productBucket = 'bonus';  
+                _search.queryParams.productBucket = 'bonus';
             }
             return vm.httpCacheGett(vm.bucketBalanceAPIUrl, _search, componentName)
-            //return vm.httpCacheOrange.gett(vm.bucketBalanceAPIUrl, {})
                 .then(function (response) {
                     return response.data;
                 })
@@ -51,6 +50,37 @@ module OrangeFeSARQ.Services{
                 });
         }
 
+
+        getBalanceByDoc(nif: string, param: string, componentName: string = 'more-megas-comp'): any {
+            let vm = this;
+            let _search: any;
+            vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalance;
+            _search = {
+                queryParams: {
+                    individualPublicId: nif,
+                    productBucket: ''
+                },
+                urlParams: ['orange', 'usageReport', 'OSP']
+            };
+
+            if (param === 'Line') {
+                _search.queryParams.productBucket = 'line';
+                //vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalanceLineAPIUrl;
+            } else if (param === 'Bonus') {
+                //_search.queryParams.productBucket = 'bonus';
+                //vm.bucketBalanceAPIUrl = vm.genericConstant.bucketBalanceBonusAPIUrl;
+                //_search = {};
+                //TO-DO: para llamar a la api
+                _search.queryParams.productBucket = 'bonus';
+            }
+            return vm.httpCacheGett(vm.bucketBalanceAPIUrl, _search, componentName)
+                .then(function (response) {
+                    return response.data;
+                })
+                .catch(function (error) {
+                    return error;
+                });
+        }
 
 
     }
