b076f9b908ccc10efa91ea6992c3d69d78614a0d
jaime_cardona <jaime_cardona>
2017-04-04 09:15:21 +0200

    "ISUE":"#FUN-5917#", "version": "1.14.0 SNAPSHOT.9", "changes":" Se mueve los servicios del contractedservices"

---
+++ src/services/productContractedTranslate.service.ts
@@ -0,0 +1,41 @@
+module contractedServices.Service {
+    'use strict';
+
+    export class ProductContractedTranslateSrv extends OrangeFeSARQ.Services.ParentService {
+        static $inject = ['$injector'];
+        public contractedServicesAPIUrl: string;
+        public searchUrl: string;
+        public genericConstant;
+
+        constructor($injector) {
+            super($injector);
+            let vm = this;
+            vm.setInjections($injector);
+            vm.contractedServicesAPIUrl = vm.genericConstant.productInventory;
+
+        }
+
+        setInjections($injector){
+            let vm = this;
+        }
+
+        getServicesContracted(msisdn: string, type: string, brand: string, detail: string, componentName: string = 'contractedServicesComp' ): any {
+            let vm = this;
+            let _search: Object = {
+                    queryParams: {
+                        publicKey: msisdn,
+                        lineCategory: type
+                    },
+                    urlParams: [brand, detail]
+            };
+            return vm.httpCacheGett(vm.contractedServicesAPIUrl, _search, componentName)
+                .then(function(response) {
+                    return response.data;
+                })
+                .catch(function(error) {
+                    return error;
+                });
+        }
+
+    }
+}
