eee6b86be33404435f84cb5b2496c23c07235697
Javier Oltra <javier.oltra.serrano.sa@everis.com>
2017-05-29 18:26:04 +0200

    MF_TEST_01_05 Test Room Invita y ahorra. Pattern de correo expresion regular erronea.

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -365,7 +365,7 @@ module OrangeFeSARQ.Services {
         checkFormatEmail(email: string): boolean {
             let vm = this;
             // - let format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
-            let format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+            let format = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
             if (email.match(format)) {
                 return true;
             }
