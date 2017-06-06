1bc22fda345af1fb0b5bfcf3091578bb33cddac4
Javier Pérez <javipg92@gmail.com>
2017-06-06 20:06:37 +0200

    Se añade queryParam en vapList

--- src/services/vapList.service.ts
+++ src/services/vapList.service.ts
@@ -7,6 +7,7 @@ module OrangeFeSARQ.Services {
         public genericConstant;
         private vapDataApiUrl;
         private brand;
+		private site: string;
 
         constructor(public $injector) {
             super($injector);
@@ -20,14 +21,17 @@ module OrangeFeSARQ.Services {
             vm.genericConstant = $injector.get('genericConstant');
             vm.vapDataApiUrl = vm.genericConstant.vapDataUrl;
             vm.brand = vm.genericConstant.brand;
+			vm.site = vm.genericConstant.site;
         }
 
         getVapData(msisdn: string, ID: string, componentName: string = 'pendingPaymentComp'): any {
             let vm = this;
             let _search: Object = {
-                queryParams: {},
+                queryParams: {
+					segment: vm.site === 'eCareEmpresas' ? 'business' : 'residential'
+				},
                 // Cuando metamos la brand, lo meteremos como primer parámetro dentro de éste array (vm.brand) y así ya funcionaría.
-                urlParams: [vm.genericConstant.brand, 'vapData', 'OSP', ID, msisdn]
+                urlParams: [vm.brand, 'vapData', 'OSP', ID, msisdn]
             };
             return vm.httpCacheGett(vm.vapDataApiUrl, _search, componentName)
                 .then(function(response) {
