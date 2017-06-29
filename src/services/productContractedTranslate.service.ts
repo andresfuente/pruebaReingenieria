bd6de7a0b755f4cae5ef0a034247752338982729
David <david.lorenzo.alvarez.sa@everis.com>
2017-06-29 09:24:56 +0200

    version:   1.18.0-SNAPSHOT.24,changes:   ProductContractedTranslate | MDW en la llamada

--- src/services/productContractedTranslate.service.ts
+++ src/services/productContractedTranslate.service.ts
@@ -1,42 +1,43 @@
 module OrangeFeSARQ.Service {
-    'use strict';
+  'use strict';
 
-    export class ProductContractedTranslateSrv extends OrangeFeSARQ.Services.ParentService {
-        static $inject = ['$injector'];
-        public contractedServicesAPIUrl: string;
-        public searchUrl: string;
-        public genericConstant;
+  export class ProductContractedTranslateSrv extends OrangeFeSARQ.Services.ParentService {
+    static $inject = ['$injector'];
+    public contractedServicesAPIUrl: string;
+    public searchUrl: string;
+    public genericConstant;
 
-        constructor($injector) {
-            super($injector);
-            let vm = this;
-            vm.setInjections($injector);
-            vm.contractedServicesAPIUrl = vm.genericConstant.productInventory;
-
-        }
-
-        setInjections($injector) {
-            let vm = this;
-        }
-
-        getServicesContracted(msisdn: string, type: string, brand: string, detail: string,
-        componentName: string = 'contractedServicesComp'): any {
-            let vm = this;
-            let _search = {
-                queryParams: {
-                    lineCategory: type,
-                    'onlyActive': vm.genericConstant.onlyActive
-                },
-                urlParams: [brand, detail, msisdn]
-            };
-            return vm.httpCacheGett(vm.contractedServicesAPIUrl, _search, componentName)
-                .then(function (response) {
-                    return response.data;
-                })
-                .catch(function (error) {
-                    return error;
-                });
-        }
+    constructor($injector) {
+      super($injector);
+      let vm = this;
+      vm.setInjections($injector);
+      vm.contractedServicesAPIUrl = vm.genericConstant.productInventory;
 
     }
+
+    setInjections($injector) {
+      let vm = this;
+    }
+
+    getServicesContracted(msisdn: string, type: string, brand: string, detail: string,
+      componentName: string = 'contractedServicesComp'): any {
+      let vm = this;
+      let _search = {
+        queryParams: {
+          lineCategory: type,
+          source: 'mdw',
+          'onlyActive': vm.genericConstant.onlyActive
+        },
+        urlParams: [brand, detail, msisdn]
+      };
+      return vm.httpCacheGett(vm.contractedServicesAPIUrl, _search, componentName)
+        .then(function(response) {
+          return response.data;
+        })
+        .catch(function(error) {
+          return error;
+        });
+    }
+
+  }
 }
