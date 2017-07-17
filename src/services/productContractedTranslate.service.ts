f3a8abc971edeffb8cd93da40e86bab3980a6309
Javier Molina <francisco.javier.molina.sanchez@everis.com>
2017-07-17 16:28:11 +0200

    correccion del productContractedTranslate para cuando llama a fijo

--- src/services/productContractedTranslate.service.ts
+++ src/services/productContractedTranslate.service.ts
@@ -22,17 +22,27 @@ module OrangeFeSARQ.Service {
     getServicesContracted(msisdn: string, type: string, brand: string, detail: string,
       componentName: string = 'contractedServicesComp', hasSource = true): any {
       let vm = this;
-      let _search = {
-        queryParams: {
-          lineCategory: type,
-          source: 'mdw',
-          'onlyActive': vm.genericConstant.onlyActive
-        },
-        urlParams: [brand, detail, msisdn]
-      };
-      if (!hasSource) {
-        delete _search.queryParams.source;
+      let _search = {};
+
+      if(type === 'fixed'){
+          _search = {
+          queryParams: {
+            lineCategory: type,
+            'onlyActive': vm.genericConstant.onlyActive
+          },
+          urlParams: [brand, detail, msisdn]
+        };
+      } else {
+        _search = {
+          queryParams: {
+            lineCategory: type,
+            source: 'mdw',
+            'onlyActive': vm.genericConstant.onlyActive
+          },
+          urlParams: [brand, detail, msisdn]
+        };
       }
+     
       return vm.httpCacheGett(vm.contractedServicesAPIUrl, _search, componentName)
         .then(function(response) {
           return response.data;
