71bfaf07fc5aa34927400fdc65bfb3d82b9975bc
Javi B <dbrivies@everis.com>
2017-11-27 20:52:50 +0100

    Actualizado services

--- src/services/getDataClient.service.ts
+++ src/services/getDataClient.service.ts
@@ -38,10 +38,11 @@ module OrangeFeSARQ.Services {
                     // Lleno el customerViewStore
                     if (response.data && response.data.user) {
                         // Eliminamos los 34 en los telefonos
-                        if ((/^34/).test(response.data.user) &&
-                         !(/[a-z A-Z]/).test(response.data.user) && 
-                        response.data.user.length === 11) {
-                                response.data.user = response.data.user.replace(/^34/, '');
+						let msisdn = response.data.user.user;
+                        if ((/^34/).test(msisdn) &&
+                         !(/[a-z A-Z]/).test(msisdn) && 
+                        msisdn.length === 11) {
+                                response.data.user.user = msisdn.replace(/^34/, '');
                         }
                         vm.customerViewStore.loginData = response.data.user;
 						if (vm.customerViewStore.info) {
