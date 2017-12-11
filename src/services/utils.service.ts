9c11b70c5b4ec70355afe0ef3b3fc0184f1156f3
carlos_garrido <juan.carlos.garrido.gonzalez@everis.com>
2017-12-11 12:04:37 +0100

    #M1ST-1897# error validación de correo

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -366,7 +366,7 @@ module OrangeFeSARQ.Services {
          */
         checkFormatEmail(email: string): boolean {
             let vm = this;
-            let format = /^[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Za-z]{2,}/;
+            let format = /^[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$/;
             if (email.match(format)) {
                 return true;
             }
