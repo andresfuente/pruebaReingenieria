dc15dd6005c66df7645be53a19c9204fd10004ac
Isabel Vera <grecia.isabel.vera.villajulca@everis.com>
2018-01-18 08:56:50 +0100

    cambiado bonusincompatibility para que se pase la respuesta del productinventory

--- src/services/product-order/productOrder.service.ts
+++ src/services/product-order/productOrder.service.ts
@@ -278,13 +278,13 @@ module OrangeFeSARQ.Services {
         });
     }
 
-    getProductBonusIncompatibility(msisdn: string, productsIdQuery: string[], componentName: string): ng.IPromise<any> {
+    getProductBonusIncompatibility(msisdn: string, products:any, componentName: string): ng.IPromise<any> {
       let vm = this;
       let BRAND = vm.genericConstant.brand;
       let METHOD = 'productBonusIncompatibility';
       let qParams = {
         msisdn,
-        productsIdQuery
+        products
       }
       let _search: Object = {
         urlParams: [BRAND, METHOD],
