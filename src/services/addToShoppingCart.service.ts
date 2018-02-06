1ae58964d4aed3ce5d988ba0d9277fa7ce96cb25
Isa Matas <mmatasra@everis.com>
2018-02-06 17:21:34 +0100

    Terminal secundario para carrito

--- src/services/addToShoppingCart.service.ts
+++ src/services/addToShoppingCart.service.ts
@@ -212,6 +212,8 @@ module OrangeFeSARQ.Services {
             let sTerminalsLength = commercialData[commercialActIndex].sTerminals ? commercialData[commercialActIndex].sTerminals.length : 0;
             let sTerminalLastId = sTerminalsLength === 0 ? 0 : commercialData[commercialActIndex].sTerminals[sTerminalsLength - 1].id;
             let selectedCartItemId;
+            let selectedCartTerminal;
+            let selectedCartRate;
             let vapCartItems = [];
             let vapCartItem;
             let unPriceItem;
@@ -219,7 +221,7 @@ module OrangeFeSARQ.Services {
             let seguro;
 
             device.itemPrice.forEach( item => {
-                if(payType === "deferred" && item.priceType === 'inicial' || item.priceType === 'cuota') {
+                if(payType === 'deferred' && item.priceType === 'inicial' || item.priceType === 'cuota') {
                     vapCartItem = {
                         'id': item.id,
                         'action': 'New',
@@ -236,7 +238,7 @@ module OrangeFeSARQ.Services {
                     };
                     vapCartItems.push(vapCartItem);
                 }
-                if (payType === "unique" && item.priceType === "unico"){
+                if (payType === 'unique' && item.priceType === 'unico'){
                     unPriceItem = item;
                 }
 
@@ -270,7 +272,7 @@ module OrangeFeSARQ.Services {
                 'id': device.siebelId,
                 'action': 'New',
                 'product': productItem,
-                'itemPrice': payType === "deferred" ? [{ 'priceType': 'aplazado' }] : unPriceItem,
+                'itemPrice': payType === 'deferred' ? [{ 'priceType': 'aplazado' }] : unPriceItem,
                 'productOffering': {
                     id: device.siebelId,
                 },
@@ -313,15 +315,18 @@ module OrangeFeSARQ.Services {
                             currentItem.sTerminals = [];
                         }
                         currentItem.sTerminals.push({ 'siebelId': device.siebelId });
-                        selectedCartItemId = currentItem.id;
+                        selectedCartTerminal = currentItem.terminalSiebelId;
+                        selectedCartRate = currentItem.rateSiebelId;
                     }
                 });
             // Se inserta el terminal secundario en el shopping cart
             if (shoppingCart !== null && shoppingCart.cartItem.length > 0) {
                 shoppingCart.cartItem.forEach(currentCartItem => {
-                    if (currentCartItem.id === selectedCartItemId) {
+                    let rate = _.find(currentCartItem.cartItem, {'id': selectedCartRate});
+                    let terminal = _.find(currentCartItem.cartItem, {'id': selectedCartTerminal});
+                    if(rate && terminal) {
                         currentCartItem.cartItem.push(secundaryDeviceCartItem);
-                        if (payType === "deferred") {
+                        if (payType === 'deferred') {
                             currentCartItem.cartItem = currentCartItem.cartItem.concat(vapCartItems);
                         }
                         if (device.insuranceSiebelId) {
