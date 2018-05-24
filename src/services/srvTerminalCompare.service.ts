c78e4dff6e695324166d0b41c66c932157474726
lgomezba <lgomezba@everis.com>
2018-05-24 10:28:50 +0200

    FCU-7791 Guardar en el terminal asociado la info de los bonos

--- src/services/srvTerminalCompare.service.ts
+++ src/services/srvTerminalCompare.service.ts
@@ -116,11 +116,13 @@ module OrangeFeSARQ.Services {
             // Se seleccionan las propiedades para session
             let deviceForSession = _.pick(device, ['terminalId', 'siebelId', 'name',
                 'description', 'litSubTitle', 'brand', 'priceType', 'insuranceSiebelId', 'srcImage',
-                'insuranceSelected', 'stock', 'isModified', 'itemPrice', 'id', 'IMEI', 'taxRate', 'taxRateName', 'tipoPago', 'eSim', 'cpDescription', 'cpSiebel', 'cpDuration']);
+                'insuranceSelected', 'stock', 'isModified', 'itemPrice', 'id', 'IMEI', 'taxRate', 'taxRateName',
+                'tipoPago', 'eSim', 'cpDescription', 'cpSiebel', 'cpDuration', 'bonusId', 'bonusDesc']);
             if (device.renewRates !== undefined && device.renewRates) {
                 deviceForSession = _.pick(device, ['terminalId', 'siebelId', 'name',
                     'description', 'litSubTitle', 'brand', 'priceType', 'insuranceSiebelId', 'srcImage',
-                    'insuranceSelected', 'stock', 'isModified', 'itemPrice', 'id', 'IMEI', 'renewRates', 'eSim', 'cpDescription', 'cpSiebel', 'cpDuration']);
+                    'insuranceSelected', 'stock', 'isModified', 'itemPrice', 'id', 'IMEI', 'renewRates', 'eSim',
+                    'cpDescription', 'cpSiebel', 'cpDuration', 'bonusId', 'bonusDesc']);
             }
 
             return deviceForSession;
