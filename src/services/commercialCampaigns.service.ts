013005cd21a45fa294ccf82d73da60632adc8e1b
Jaime Alain <jdecorti@everis.com>
2018-06-14 18:19:27 +0200

    Modificando los headers para las campañas, locationName

--- src/services/commercialCampaigns.service.ts
+++ src/services/commercialCampaigns.service.ts
@@ -11,18 +11,36 @@ module OrangeFeSARQ.Services {
         static $inject = ['$injector'];
 
         public commercialCampaignsAPIUrl: string;
+        private storeProvince: string;
 
         constructor(public $injector) {
             super($injector);
             let vm = this;
 
             vm.setInjections($injector);
+            vm.setStoreProvince();
         }
 
         setInjections($injector) {
             let vm = this;
 
             vm.commercialCampaignsAPIUrl = vm.genericConstant.commercialCampaigns;
+        }
+        /**
+         * @ngdoc method
+         * @name ratesParent.Services:RatesParentSrv#getStoreProvince
+         * @methodOf ratesParent.Services:MosaicFileSrv
+         * @description
+         * Establece el nombre de la provincia de la tienda  
+         */
+        setStoreProvince() {
+            let vm = this;
+            let shopInfo = JSON.parse(sessionStorage.getItem('shopInfo'));
+            if (shopInfo && shopInfo.province && shopInfo.province !== '') {
+                vm.storeProvince = shopInfo.province;
+            } else {
+                vm.storeProvince = 'MADRID';
+            }
         }
 
         /**
@@ -34,26 +52,30 @@ module OrangeFeSARQ.Services {
          * @description Obtiene los datos de las campañas comerciales de un cliente
          * @returns {object} Devuelve una promesa con el response.
          */
-        getComercialCampaings85205(individualPublicId, locationName, comp: string) {
+        getComercialCampaings85205(individualPublicId, comp: string) {
             let vm = this;
             let _search: Object = {
                 queryParams: {
-                    'individualPublicId': individualPublicId,
-                    'locationName': locationName
+                    'individualPublicId': individualPublicId
                 },
                 urlParams: [vm.genericConstant.brand, 'getComercialCampaings85205']
             };
-            return vm.httpCacheGett(vm.commercialCampaignsAPIUrl, _search, comp , true)
+
+            // CABECERA HASHMAP
+            let _headers = new HashMap<string, string>();
+            _headers.set('locationName', _.deburr(vm.storeProvince.toUpperCase()));
+
+            return vm.httpCacheGeth(vm.commercialCampaignsAPIUrl, _search, _headers, comp, true)
                 .then((response) => {
                     return response.data;
-                    }, (err) => {
-                        throw err;
-                    }
+                }, (err) => {
+                    throw err;
+                }
                 );
         }
     }
-    
-// Registration
-angular.module('commercialCampaignsSrv', [])
-.service('commercialCampaignsSrv', OrangeFeSARQ.Services.CommercialCampaignsSrv);
+
+    // Registration
+    angular.module('commercialCampaignsSrv', [])
+        .service('commercialCampaignsSrv', OrangeFeSARQ.Services.CommercialCampaignsSrv);
 }
