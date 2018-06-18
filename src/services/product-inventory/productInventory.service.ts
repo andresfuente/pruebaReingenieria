b41228f1bed01af9750346cf3070ed2d3aba0e43
Isa Matas <mmatasra@everis.com>
2018-06-18 09:22:24 +0200

    Revert Servicio productInventory para Jazztel

--- src/services/product-inventory/productInventory.service.ts
+++ src/services/product-inventory/productInventory.service.ts
@@ -20,9 +20,9 @@ module OrangeFeSARQ.Services {
       vm.utils = $injector.get('utils');
     }
 
-    getServicesContracted(msisdn: string, componentName: string = 'productInventorySrv', refresh: boolean = false, anotherBrand: string = this.genericConstant.brand): any {
+    getServicesContracted(msisdn: string, componentName: string = 'productInventorySrv', refresh: boolean = false): any {
       let vm = this;
-      let BRAND = anotherBrand;
+      let BRAND = vm.genericConstant.brand;
       let METHOD = 'services';
       let type = vm.utils.isFixedLine(msisdn) ? 'fixed' : 'mobile';
       let request;
