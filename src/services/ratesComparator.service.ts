e6436a4f7e3739593e2b0d9af866526a39c72190
Daniel Gamo <dgamoalo@everis.com>
2018-07-27 14:44:16 +0200

    Otro parámetro "covergente"

--- src/services/ratesComparator.service.ts
+++ src/services/ratesComparator.service.ts
@@ -403,7 +403,7 @@ module OrangeFeSARQ.Services {
                 defaultTechnology: defaultTechnology
             };
  
-            if (categoryParam !== 'Covergente' || defaultTechnology === 'Y') {
+            if (categoryParam !== 'Convergente' || defaultTechnology === 'Y') {
                 delete params.idTecnologiaList;
             }
 
