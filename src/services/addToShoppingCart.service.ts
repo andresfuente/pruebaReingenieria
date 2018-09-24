8827003b303cb755feaf41dc6c0999b268afab75
pcordonc <pcordonc@everis.com>
2018-09-24 10:35:16 +0200

    FDCO-6305 PRO variable guardado terminal secundario

--- src/services/addToShoppingCart.service.ts
+++ src/services/addToShoppingCart.service.ts
@@ -15,6 +15,7 @@ module OrangeFeSARQ.Services {
         public clientJazztelSrv: OrangeFeSARQ.Services.ClientJazztelSrv;
         public userSrv: OrangeFeSARQ.Services.UserSrv;
         public data;
+        public shoppingCartAux;
 
         /**
          * @ngdoc method
