a206defd0f15941e7a035e3a6594b09b1d40671b
USERSAD\rsanchfu <raul.sanchez.fuentes@everis.com>
2019-04-03 12:01:22 +0200

    FIX: Contractible

--- src/services/productCatalogV2Srv.service.ts
+++ src/services/productCatalogV2Srv.service.ts
@@ -90,7 +90,8 @@ module OrangeFeSARQ.Services {
                     commercialAction: commercialAction,
                     productType: productType,
                     isExistingCustomer: isExistingCustomer,
-                    idOfertaComercialList: idOfertaComercialList
+                    idOfertaComercialList: idOfertaComercialList,
+                    ospContractible: 'Y'
                 },
                 urlParams: [vm.genericConstant.brand, 'productSpecificationv2View/OSP']
             };
