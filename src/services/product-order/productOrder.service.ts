5de27db62489cb7322a6591717a8aa6c8c5194c2
María Sánchez <maria.sanchez.carvajal@everis.com>
2019-05-23 17:30:50 +0200

    fix: INC000000776127 compruebo status en el metodo bueno

--- src/services/product-order/productOrder.service.ts
+++ src/services/product-order/productOrder.service.ts
@@ -203,7 +203,7 @@ module OrangeFeSARQ.Services {
 
       return vm.httpPost(vm.urlProductOrder, _search, componentName)
         .then(function (success) {
-          if (success.status == 202 || success.status == 422 || success.status == 403) {
+          if (success.status == 202 || success.status == 422) {
             return success;
           }
           else {
@@ -354,7 +354,7 @@ module OrangeFeSARQ.Services {
         .then((response) => {
           let _resp = response.data;
           let status = response.status;
-          if (status == 202 || status == 422) {
+          if (status == 202 || status == 422 || status == 403) {
             return response;
           }
           if (_resp.error) {
