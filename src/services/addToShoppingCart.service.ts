efed8c1174689f6015fb091fc4a16a38d4123242
Aaron Isabel <aaron.isabel.diaz@gmail.com>
2019-05-30 17:50:02 +0200

    fix: soho

--- src/services/addToShoppingCart.service.ts
+++ src/services/addToShoppingCart.service.ts
@@ -514,7 +514,7 @@ module OrangeFeSARQ.Services {
                     commercialData[commercialActIndex].NACRateInShoppingCart = true;
                 }
                 //Seteamos el valor de SOHORateInShoppingCart cuando groupNme es movil, cuando typeService es movil y cuando el segmento es empresa
-                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && clientData.ospCustomerSegment === 'empresa') {
+                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && (clientData.ospCustomerSegment === 'empresa' || clientData.ospCustomerSegment === 'autonomo') ) {
                     commercialData[commercialActIndex].SOHORateInShoppingCart = true;
                 }
 
@@ -1111,7 +1111,7 @@ module OrangeFeSARQ.Services {
                 }
                 //Seteamos el valor de SOHORateInShoppingCart cuando groupNme es movil, cuando typeService es movil y cuando el segmento es empresa
                 //TODO comprobar que se setea correctamente SOHORateInShoppingCart
-                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && clientData.ospCustomerSegment === 'empresa') {
+                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && (clientData.ospCustomerSegment === 'empresa' || clientData.ospCustomerSegment === 'autonomo') ) {
                     commercialData[commercialActIndex].SOHORateInShoppingCart = true;
                 }
 
