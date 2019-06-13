92e2a79ed35bda347f81ffa4b13b2f72525adbfe
Juan Antonio Barroso <juan.antonio.barroso.gomez.platero@everis.com>
2019-06-13 19:58:20 +0200

    FEAT: Se añade test unitario para generación fix

--- laGuerraDeLosClones.bat
+++
@@ -1,1 +0,0 @@
-mklink /D ".\node_modules" "C:\Dev\FichaCliente\FDC_DEV_VERSION\FDC_ANG_SPA\node_modules\" 
---
+++ test/src/addToShoppingCart.service.test.ts
@@ -0,0 +1,68 @@
+module addToShoppingCart.Test.Services {
+  describe('addToShoppingCart', () => {
+      let service: OrangeFeSARQ.Services.AddToShoppingCartSrv;
+
+      beforeEach(() => {
+          // includeDeps();
+          angular.mock.module('orange-arq-common');
+          angular.mock.module('servicesCommons');
+          angular.mock.module('genericConstant');
+
+      });
+
+      beforeEach(angular.mock.inject((_addToShoppingCartSrv_) => {
+          service = _addToShoppingCartSrv_;
+      }));
+
+      beforeEach(() => {
+          jasmine.getJSONFixtures().fixturesPath = 'base/test/mock';
+          
+      });
+      afterEach(() => jasmine.getJSONFixtures().clearCache());
+
+      describe('injections', () => {
+          it('should inject correctly', () => {
+              expect(service).toBeDefined();
+
+          });
+      });
+
+      describe('injections', () => {
+        it('should inject correctly', () => {
+            service.getBundle();
+
+        });
+    });
+
+    describe('injections', () => {
+      it('should inject correctly', () => {
+        let a = [{
+          name: '123'
+        }]
+        service.getCVBundleData(a);
+
+      });
+  });
+
+       /**
+   * @ngdoc test pack
+   * @author Aarón Isabel Díaz (aisabeld)
+   * @methodOf addToShoppingCart.Services:addToShoppingCartSrv
+   * @description
+   * Conjunto de test para el servicio addToShoppingCartSrv
+   */
+      // describe('Service to addToShoppingCartSrv ', () => {
+          
+
+      //     describe('- SOHORateInShoppingCart()', () => {
+      //         it('commercialData && commercialData.length', function () {
+      //             service.SOHORateInShoppingCart();
+      //             expect(service.SOHORateInShoppingCart).toBeEmpty;
+      //         });
+              
+      //     });
+
+
+      // });
+  });
+}
