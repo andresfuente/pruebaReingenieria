d6ebc8a2b253d5f05d7aa8fcdbbeae6c0b98c06b
nadermohammad <nader.mohammad.zaiter.sa@everis.com>
2019-06-20 17:13:34 +0200

    FIX: modificamos modelo mosaico de terminales para mostrar precios jazztel

--- src/models/mosaicFile.model.ts
+++ src/models/mosaicFile.model.ts
@@ -219,6 +219,7 @@ module mosaicFile.Models {
         public anchor: string[] = [];
         private renewRates = [];
         private prepaidRenewPrices = [];
+        public brand: any = sessionStorage.getItem('pangea-brand') ? sessionStorage.getItem('pangea-brand') : 'orange';
 
         constructor(serviceData: any, ospCustomerSegment: string, priceName: string, mosaicFileCompOWCSStore?: any) {
 
@@ -458,15 +459,27 @@ module mosaicFile.Models {
                                     // Añadiendo el precio al arreglo de precios del terminal
                                     this.itemPrice.push(priceItem);
 
+                                    let if1, if2, if3;
+
+                                    if (this.brand == 'jazztel') {
+                                        if1 = 'Importe prepago';
+                                        if2 = 'Importe recurrente';
+                                        if3 = 'Importe alta';
+                                    } else {
+                                        if1 = 'inicial';
+                                        if2 = 'cuota';
+                                        if3 = 'unico';
+                                    }
+
                                     if (price.name === priceName) {
-                                        if (price.priceType && price.priceType === 'inicial') {
+                                        if (price.priceType && price.priceType === if1) {
                                             if (ospCustomerSegment.toLocaleLowerCase() === 'residencial') {
                                                 this.initialPaid = price.Price.taxIncudedAmount;
                                             } else {
                                                 this.initialPaid = price.Price.dutyFreeAmount;
                                             }
                                         }
-                                        if (price.priceType && price.priceType === 'cuota') {
+                                        if (price.priceType && price.priceType === if2) {
                                             this.litDeadlines = price.applicationDuration;
                                             if (ospCustomerSegment.toLocaleLowerCase() === 'residencial') {
                                                 this.litPrice = price.Price.taxIncudedAmount;
@@ -478,7 +491,7 @@ module mosaicFile.Models {
                                                 this.totalPaid = this.initialPaid + this.litPrice * this.litDeadlines;
                                             }
                                         }
-                                        if (price.priceType && price.priceType === 'unico') {
+                                        if (price.priceType && price.priceType === if3) {
                                             if (ospCustomerSegment.toLocaleLowerCase() === 'residencial') {
                                                 this.uniquePaid = price.Price.taxIncudedAmount;
                                             } else {
