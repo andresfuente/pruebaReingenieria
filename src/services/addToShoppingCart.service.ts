85059f13fc6719642412a3293bd63e7abfd4b07d
Jaime Molina <jaimedurcal@hotmail.com>
2019-07-02 12:46:24 +0200

    fix: session soho in shoppingcart

--- src/services/addToShoppingCart.service.ts
+++ src/services/addToShoppingCart.service.ts
@@ -530,7 +530,7 @@ module OrangeFeSARQ.Services {
                 }
                 //Seteamos el valor de SOHORateInShoppingCart cuando groupNme es movil, cuando typeService es movil y cuando el segmento es empresa
                 //TODO comprobar que se setea correctamente SOHORateInShoppingCart
-                if (rate.groupName === 'movil' && rate.typeService === 'movil' && clientData.ospCustomerSegment === 'empresa') {
+                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && (clientData.ospCustomerSegment === 'empresa' || clientData.ospCustomerSegment === 'autonomo')) {
                     commercialData[commercialActIndex].SOHORateInShoppingCart = true;
                 }
 
@@ -1127,7 +1127,7 @@ module OrangeFeSARQ.Services {
                 }
                 //Seteamos el valor de SOHORateInShoppingCart cuando groupNme es movil, cuando typeService es movil y cuando el segmento es empresa
                 //TODO comprobar que se setea correctamente SOHORateInShoppingCart
-                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && clientData.ospCustomerSegment === 'empresa') {
+                if (rate.groupName === 'Mobile Only_NAC' && rate.typeService === 'movil' && (clientData.ospCustomerSegment === 'empresa' || clientData.ospCustomerSegment === 'autonomo')) {
                     commercialData[commercialActIndex].SOHORateInShoppingCart = true;
                 }
 
